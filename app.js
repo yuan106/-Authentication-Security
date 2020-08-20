64d75b6b20b299894dc8a445c212be3caa7d64fa
yuan106 <61606121+yuan106@users.noreply.github.com>
2020-08-20 16:07:12 -0700

    hashing passwords

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5"); //hashing passwords
 
 const app = express();
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.set("view engine", "ejs");
 app.use(
@@ -30,10 +32,10 @@ const userSchema = new mongoose.Schema({
 });
 //encryption
 // const secret = "this is secret";
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -51,7 +53,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password), //hashing passwords
   });
   newUser.save(function (err) {
     if (err) {
@@ -64,7 +66,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   // in collection of User
   User.findOne(
